a918cd4f14d9609fb72c0cff7ff5f8cfd746eac5
Ganesh Keerthi <sganeshkeerthi@gmail.com>
2025-02-21 05:28:48 +0530

    Master :: Main Application :: new line :: Commit

--- src/main/java/com/suggi/MainApplication.java
+++ src/main/java/com/suggi/MainApplication.java
@@ -3,5 +3,6 @@ package com.suggi;
 public class MainApplication {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		System.out.println("Hi I am Ganesh Keerthi");
 	}
 }
