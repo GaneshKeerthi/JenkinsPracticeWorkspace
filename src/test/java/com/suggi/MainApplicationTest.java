da1970d611de882fcaffb1725a57ebe02ac3cec2
Ganesh Keerthi <sganeshkeerthi@gmail.com>
2025-02-21 05:08:58 +0530

    first commit of a maven project

---
+++ src/test/java/com/suggi/MainApplicationTest.java
@@ -0,0 +1,12 @@
+package com.suggi;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class MainApplicationTest {
+	@Test
+	public void display() {
+		assertEquals(true, true);
+	}
+}
