a01ee04c97021a7c88cec42efbc7361e5a394e73
Ganesh Keerthi <sganeshkeerthi@gmail.com>
2025-02-21 06:15:06 +0530

    Master :: Main Application :: new line 2:: Commit

--- src/main/java/com/suggi/MainApplication.java
+++ src/main/java/com/suggi/MainApplication.java
@@ -4,5 +4,6 @@ public class MainApplication {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("Hi I am Ganesh Keerthi");
+		System.out.println("Hi I am a line to check Build pipeline in Jenkins");
 	}
 }
