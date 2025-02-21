6301bdb9b9531304e43eb2ddc68d6fd0238fa0c8
Ganesh Keerthi <89194065+GaneshKeerthi@users.noreply.github.com>
2025-02-21 12:05:22 +0530

    Rectified the CTE

--- src/main/java/com/suggi/MainApplication.java
+++ src/main/java/com/suggi/MainApplication.java
@@ -4,6 +4,6 @@ public class MainApplication {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("Hi I am Ganesh Keerthi");
-		System.out.println("Hi I am a line to check Build pipeline in Jenkins")
+		System.out.println("Hi I am a line to check Build pipeline in Jenkins");
 	}
 }
